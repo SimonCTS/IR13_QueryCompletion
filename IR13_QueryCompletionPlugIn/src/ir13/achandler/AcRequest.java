bd84aae270742ea7a16aa957959f1f50f7a15114
Hobbe Wärnegård <hobrik@hobrik.se>
2013-04-29 23:22:41 +0200

    AutoCompleteHandler and AxRequest modified

--- IR13_QueryCompletionPlugIn/src/ir13/achandler/AcRequest.java
+++ IR13_QueryCompletionPlugIn/src/ir13/achandler/AcRequest.java
@@ -1,17 +1,40 @@
 package ir13.achandler;
 
 public class AcRequest {
+	public static byte REGULAR = 1;
+	public static byte FIELD = 2;
+	public static byte SYNTAX = 4;
+	
 	private String field;
 	private String content;
+	private byte reqType;
 	
-	public AcRequest(String field, String content) {
+	public AcRequest(byte requestType, String field, String content) {
 		super();
+		reqType = requestType;
 		this.field = field;
 		this.content = content;
 	}
 	
-	public boolean hasContent() {
-		return content != null;
+	public AcRequest(byte requestType, String field) {
+		super();
+		reqType = requestType;
+		this.field = field;
+		this.content = null;
+	}
+	
+	public byte requestType() {
+		return reqType;
+	}
+	
+	public boolean isRegularRequest() {
+		return (reqType ^ REGULAR) == 0;
+	}
+	public boolean isFieldRequest() {
+		return (reqType ^ FIELD) == 0;
+	}
+	public boolean isSyntaxRequest() {
+		return (reqType ^ SYNTAX) == 0;
 	}
 	
 	public String getField() {
